5e6ab408d7107a9ab95b45fd07676f896dc7b92b
m4ciek444 <m4ci333k@gmail.com>
2025-04-30 13:07:04 +0200

    Dodane Makefile, poprawiono to co zglosil lint

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,10 +1,12 @@
-from hello_world import app
 from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
+from hello_world import app # noqa
+
 
 moje_imie = "Maciek"
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
@@ -13,6 +15,7 @@ def index():
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
